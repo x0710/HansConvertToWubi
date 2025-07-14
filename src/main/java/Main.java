fd94df10240f6064e92fea3ac640b5e786781071
x0710 <x0710_x0710@outlook.com>
2025-07-14 17:43:03 +0800

    新增可选带一级简码功能

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,32 +5,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        toConvert();
+    }
+    private static void toConvert() throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("请输出要被转化文件，支持路径\n");
+        System.out.println("请输出要被转化文件，支持路径");
         File path = new File(sc.next());
+        System.out.println("是否追加一级简码？(y/n)");
+        boolean isAddSimple = sc.next().toLowerCase().matches("y");
         if(path.isDirectory()) {
             // 路径，
             System.out.println("输入的为路径");
             for(File f:path.listFiles()) {
                 if(f.isDirectory()) continue;
-                parseFile(f);
+                parseFile(f, isAddSimple);
             }
         }else {
             try {
-                parseFile(path);
+                parseFile(path, isAddSimple);
             }catch (FileNotFoundException e) {
                 System.err.println("文件不存在！");
             }
         }
 
-
     }
-    public static void parseFile(final File f) throws IOException {
-        final File INPUT_FILE = f;
-        final File OUTPUT_PATH = new File(f.getAbsolutePath()+"-Conver");
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH, false));
+    /**
+     * 将一个文件的词语转化为五笔编码
+     * 输入文件的格式必须为{{词语 词频 }}，中间用空白符分隔
+     * 输出文件与输入文件同目录，名称为输入文件.cov，格式为{{词语\t五笔编码\t词频}}
+     * @param f 输入文件
+     * @param isAppendLvl1 是否在输出文件头部增加一级五笔简码
+     * @throws IOException 发生IO错误
+     */
+    public static void parseFile(final File f, boolean isAppendLvl1) throws IOException {
+        BufferedReader l1br = null;
+        if(isAppendLvl1) l1br = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("Level1Code")));
+
+        final File INPUT_FILE = f;
+        final File OUTPUT_PATH = new File(f.getAbsolutePath()+".cov");
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH, true));
         BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE));
+
+        // 写一级简码
+        for(String buf;isAppendLvl1&&(buf=l1br.readLine())!=null;) {
+            String[] tmp = buf.split("\\s");
+            bw.write(tmp[0]+"\t"+tmp[1]+"\t");
+            bw.newLine();
+        }
+        if(l1br != null) l1br.close();
 
         int count = 0;
         String hans = null; // 接下来要被转换成五笔编码的词组
--- src/main/java/ReadSystem.java
+++ src/main/java/ReadSystem.java
@@ -12,20 +12,42 @@ import java.util.HashMap;
 public class ReadSystem {
     private static ReadSystem instance;
 
-    private final File topDir;
-    private final HashMap<Character, char[]> hansMap;
+    // 存储五笔编码
+    private final HashMap<Character, char[]> hansHashMap;
+    // 存储词频
+    private final HashMap<String, Integer> hansPriority;
+
+    static {
+        instance = new ReadSystem();
+    }
+
     private ReadSystem() {
-        topDir = new File(this.getClass().getResource("UnicodeCJK-WuBi/").getFile());
-        hansMap = new HashMap<>(30000);
-        try (BufferedReader br = new BufferedReader(new FileReader(new File(topDir, "CJK.txt")))) {
+        hansPriority = new HashMap<>(35000);
+        hansHashMap = new HashMap<>(21000);
+
+        // 加载对照表
+        File wubiRawcode = new File(this.getClass().getResource("UnicodeCJK-WuBi/").getFile());
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(wubiRawcode, "CJK.txt")))) {
             for(String buf;(buf=br.readLine())!=null;) {
                 String[] bufSplit = buf.split(",");
-                hansMap.put(bufSplit[1].charAt(0), bufSplit[2].toCharArray());
+                hansHashMap.put(bufSplit[1].charAt(0), bufSplit[2].toCharArray());
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+
+        // 加载词频文件
+        File priorityFile = new File(this.getClass().getResource("priority.dict").getFile());
+        try (BufferedReader br1 = new BufferedReader(new FileReader(priorityFile))) {
+            for(String buf;(buf=br1.readLine())!=null;) {
+                String[] bufSplit = buf.split("\\s");
+                hansPriority.put(bufSplit[0], Integer.parseInt(bufSplit[1]));
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -37,15 +59,8 @@ public class ReadSystem {
      * @throws NotHansException 若该字不为汉字，或不在U+4E00到U+9FFF区间
      */
     public static String getFullCode(char hans) throws NotHansException {
-        if(instance == null) {
-            synchronized (ReadSystem.class) {
-                if(instance == null) {
-                    instance = new ReadSystem();
-                }
-            }
-        }
         if(hans < 0x4e00 || hans > 0x9fff) throw new NotHansException(hans);
-        return String.valueOf(instance.hansMap.get((char)hans));
+        return String.valueOf(instance.hansHashMap.get((char)hans));
     }
 
     /**
@@ -70,4 +85,18 @@ public class ReadSystem {
         String full = getFullCode(hans);
         return full.substring(full.length()-backNum);
     }
+
+    /**
+     * 获得某个词汇的词频
+     * @param val 目标汉字
+     * @return 汉字词频，如果没有结果返回0
+     */
+    public static int getPriority(String val) {
+        try {
+            return instance.hansPriority.get(val);
+        }catch (NullPointerException e) {
+            return 0;
+        }
+    }
+
 }
