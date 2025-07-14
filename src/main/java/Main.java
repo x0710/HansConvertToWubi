92b7074a88fd57c0b7a23c5280f313c976407da2
x0710 <x0710_x0710@outlook.com>
2025-07-14 19:17:58 +0800

    新增“最简编码”功能

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import exception.NotHansException;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +11,8 @@ public class Main {
     private static void toConvert() throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输出要被转化文件，支持路径");
-        File path = new File(sc.next());
+//        File path = new File(sc.next());
+        File path = new File("/home/x0710/Desktop/priority.dict");
         System.out.println("是否追加一级简码？(y/n)");
         boolean isAddSimple = sc.next().toLowerCase().matches("y");
         if(path.isDirectory()) {
@@ -18,11 +20,11 @@ public class Main {
             System.out.println("输入的为路径");
             for(File f:path.listFiles()) {
                 if(f.isDirectory()) continue;
-                parseFile(f, isAddSimple);
+                parseFile(f, isAddSimple, true);
             }
         }else {
             try {
-                parseFile(path, isAddSimple);
+                parseFile(path, isAddSimple, true);
             }catch (FileNotFoundException e) {
                 System.err.println("文件不存在！");
             }
@@ -36,25 +38,31 @@ public class Main {
      * 输出文件与输入文件同目录，名称为输入文件.cov，格式为{{词语\t五笔编码\t词频}}
      * @param f 输入文件
      * @param isAppendLvl1 是否在输出文件头部增加一级五笔简码
+     * @param simplize 是否启用写入最简五笔，但在写入最简码后，也总是会写入一遍原码
      * @throws IOException 发生IO错误
      */
-    public static void parseFile(final File f, boolean isAppendLvl1) throws IOException {
+    public static void parseFile(final File f, final boolean isAppendLvl1, final boolean simplize) throws IOException {
         BufferedReader l1br = null;
         if(isAppendLvl1) l1br = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("Level1Code")));
 
         final File INPUT_FILE = f;
         final File OUTPUT_PATH = new File(f.getAbsolutePath()+".cov");
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH, true));
+        HashSet<String> writted = new HashSet<>();
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_PATH, false));
         BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE));
 
         // 写一级简码
         for(String buf;isAppendLvl1&&(buf=l1br.readLine())!=null;) {
             String[] tmp = buf.split("\\s");
             bw.write(tmp[0]+"\t"+tmp[1]+"\t");
+            writted.add(tmp[1]);
             bw.newLine();
         }
         if(l1br != null) l1br.close();
+
+
 
         int count = 0;
         String hans = null; // 接下来要被转换成五笔编码的词组
@@ -64,8 +72,21 @@ public class Main {
             count++;
             hans = split[0];
             try {
-                String wubiCode = TransSystem.getWubiCode(hans);
-                bw.write(hans+"\t"+wubiCode+"\t"+split[1]);
+                String wubicode = TransSystem.getWubiCode(hans),
+                        raw = wubicode;
+                StringBuilder wb = new StringBuilder(wubicode);
+                for(;simplize;) { // 尽力缩减首先进入转换词语的重叠，保证输出质量
+                    if(writted.contains(wb.toString())) {
+                        writted.add(wubicode);
+                        bw.write(hans+"\t"+wubicode+"\t"+split[1]);
+                        bw.newLine();
+                        break;
+                    }else {
+                        wubicode = wb.toString();
+                        wb = wb.deleteCharAt(wb.length()-1);
+                    }
+                }
+                bw.write(hans+"\t"+raw+"\t"+split[1]);
                 bw.newLine();
             }
             catch (NotHansException e) {
