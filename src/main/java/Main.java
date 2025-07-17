0169ec37f740c70246c8507aaab3fa401c72e283
x0710 <x0710_x0710@outlook.com>
2025-07-17 09:54:59 +0800

    修复同码生成多行的bug，新增二级简码输出

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,8 @@ public class Main {
     private static void toConvert() throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输出要被转化文件，支持路径");
-//        File path = new File(sc.next());
-        File path = new File("/home/x0710/Desktop/priority.dict");
+        File path = new File(sc.next());
+//        File path = new File("/home/x0710/Desktop/priority.dict");
         System.out.println("是否追加一级简码？(y/n)");
         boolean isAddSimple = sc.next().toLowerCase().matches("y");
         if(path.isDirectory()) {
@@ -37,13 +37,17 @@ public class Main {
      * 输入文件的格式必须为{{词语 词频 }}，中间用空白符分隔
      * 输出文件与输入文件同目录，名称为输入文件.cov，格式为{{词语\t五笔编码\t词频}}
      * @param f 输入文件
-     * @param isAppendLvl1 是否在输出文件头部增加一级五笔简码
+     * @param isAppendlvl12 是否在输出文件头部增加一、二级五笔简码
      * @param simplize 是否启用写入最简五笔，但在写入最简码后，也总是会写入一遍原码
      * @throws IOException 发生IO错误
      */
-    public static void parseFile(final File f, final boolean isAppendLvl1, final boolean simplize) throws IOException {
+    public static void parseFile(final File f, final boolean isAppendlvl12, final boolean simplize) throws IOException {
         BufferedReader l1br = null;
-        if(isAppendLvl1) l1br = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("Level1Code")));
+        BufferedReader l2br = null;
+        if(isAppendlvl12) {
+            l1br = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("Level1Code")));
+            l2br = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("Level2Code")));
+        }
 
         final File INPUT_FILE = f;
         final File OUTPUT_PATH = new File(f.getAbsolutePath()+".cov");
@@ -54,7 +58,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE));
 
         // 写一级简码
-        for(String buf;isAppendLvl1&&(buf=l1br.readLine())!=null;) {
+        for(String buf;isAppendlvl12&&(buf=l1br.readLine())!=null;) {
             String[] tmp = buf.split("\\s");
             bw.write(tmp[0]+"\t"+tmp[1]+"\t");
             writted.add(tmp[1]);
@@ -62,6 +66,14 @@ public class Main {
         }
         if(l1br != null) l1br.close();
 
+        // 写二级简码
+        for(String buf;isAppendlvl12&&(buf=l2br.readLine())!=null;) {
+            String[] tmp = buf.split("\\s");
+            bw.write(tmp[0]+"\t"+tmp[1]+"\t");
+            writted.add(tmp[1]);
+            bw.newLine();
+        }
+        if(l2br != null) l2br.close();
 
 
         int count = 0;
@@ -86,8 +98,10 @@ public class Main {
                         wb = wb.deleteCharAt(wb.length()-1);
                     }
                 }
-                bw.write(hans+"\t"+raw+"\t"+split[1]);
-                bw.newLine();
+                if(!wubicode.equals(raw)) { // 如果原码和简写码相同，就不再写，防止同码出现两行相同结果
+                    bw.write(hans+"\t"+raw+"\t"+split[1]);
+                    bw.newLine();
+                }
             }
             catch (NotHansException e) {
                 System.err.printf("提示：在第%d行，文本“%s”无法转换！\n", count, hans);
